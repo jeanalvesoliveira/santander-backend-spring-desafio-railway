a7afbbc205ceaa344434d34bb241eda6f2c8b3d0
Jean Alves de Oliveira <Jean@DESKTOP-OFKD5L4>
2024-07-11 09:19:59 -0300

    First commit

---
+++ src/main/java/br/com/jean/app/SantanderBackendSpringDesafioRailwayApplication.java
@@ -0,0 +1,13 @@
+package br.com.jean.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBackendSpringDesafioRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBackendSpringDesafioRailwayApplication.class, args);
+	}
+
+}
